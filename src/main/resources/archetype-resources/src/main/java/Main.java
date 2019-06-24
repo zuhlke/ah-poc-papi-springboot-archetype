de4664e13e7d04f3295052e880ae35b28d2845a0
romo <robert.moore@zuhlke.com>
2019-06-24 15:31:24 +0100

    formatting

--- src/main/resources/archetype-resources/src/main/java/Main.java
+++ src/main/resources/archetype-resources/src/main/java/Main.java
@@ -2,8 +2,8 @@ package $package;
 
 /*
     This is the entry point of the program. It is called Main because the first thing
-    you learn in Java is that `Main` is the entry point. This is one example of
-    "The Power of Sameness".
+    you learn in Java is that `Main` is the entry point.
+    This is one example of "The Power of Sameness".
 */
 public class Main {
     public static void main(String[] args) {
