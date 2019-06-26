9680d4e7b0ad7585b9e5bb368401776bcd2ac730
romo <robert.moore@zuhlke.com>
2019-06-26 10:53:20 +0100

    Add reactive calls/webflux to the archetype. Add four methods: [reactive|blocking][get|post] to the RestClient object. Added an informative comment to the SpringbootApplication class.

--- src/main/resources/archetype-resources/src/main/java/SpringbootApplication.java
+++ src/main/resources/archetype-resources/src/main/java/SpringbootApplication.java
@@ -5,6 +5,7 @@ import ${package}.api.RestClient;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import static org.springframework.boot.SpringApplication.*;
 
@@ -12,14 +13,18 @@ import static org.springframework.boot.SpringApplication.*;
     This class contains the Spring entry point of the API. By providing the static
     methods `start` and `stop`, the program's top-level functionality can be controlled
     easily within integration tests.
+    All the dependencies required to run this spring application can be injected at this level,
+    meaning that the whole system can be exercised with any component being stubbed or mocked. The
+    effect is that this API is extremely easy to write automated tests for and reason about, which
+    greatly decreases the cost of change.
 */
 @SpringBootApplication
 public class SpringbootApplication {
-    public static final String LOG_ID = "log_id";
+    public static final String LOG_ID = "${app-name}";
     private static ConfigurableApplicationContext context;
 
     public static void start(String[] args) {
-        SpringController.initialise(new RequestHandler(new RestClient()));
+        SpringController.initialise(new RequestHandler(new RestClient(WebClient.create())));
         context = run(SpringbootApplication.class, args);
     }
 
--- src/main/resources/archetype-resources/src/main/java/api/RestClient.java
+++ src/main/resources/archetype-resources/src/main/java/api/RestClient.java
@@ -1,5 +1,10 @@
 package ${package}.api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
 /*
     This class contains our implementation of a REST interface with backend systems.
 
@@ -7,4 +12,35 @@ package ${package}.api;
     requirements around performance, security and reliability.
 */
 public class RestClient {
+    private final WebClient webClient;
+
+    public RestClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public <T> Mono<T> reactiveGet(String url, Class<T> bodyType) {
+        return webClient.get()
+                .uri(url)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .onStatus(HttpStatus::isError, response -> Mono.error(new Exception("Request 'GET " + url + "' gave response with status code" + response.statusCode().value())))
+                .bodyToMono(bodyType);
+    }
+
+    public <T> T blockingGet(String url, Class<T> bodyType) {
+        return reactiveGet(url, bodyType).block();
+    }
+
+    public <T> Mono<T> reactivePost(String url, Class<T> bodyType) {
+        return webClient.post()
+                .uri(url)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .onStatus(HttpStatus::isError, response -> Mono.error(new Exception("Request 'GET " + url + "' gave response with status code" + response.statusCode().value())))
+                .bodyToMono(bodyType);
+    }
+
+    public <T> T blockingPost(String url, Class<T> bodyType) {
+        return reactivePost(url, bodyType).block();
+    }
 }
